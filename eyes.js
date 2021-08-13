bf890395d3a495883072baab21bc443c1371443b
no-dice-what-a-drag <88875165+no-dice-what-a-drag@users.noreply.github.com>
2021-08-13 03:01:55 -0400

    Add files via upload

--- eyes.js
+++ eyes.js
@@ -5,13 +5,6 @@ document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
-  // Im not really sure how i would use for loops here
-  // This is here to make the tester happy...
-  for (var i = 0; i < 1; ++i)
-  {
-    var a = 1;
-  }
-
   ball1[0].style.left = x;
   ball1[0].style.top = y;
   ball1[0].transform = 'translate(-' + x + ',-' + y + ')';
